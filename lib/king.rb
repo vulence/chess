b1e8c3a7f191d5e5efe9e7f1712e4f18b56942ad
vulence <vukasinmarinkovic0@gmail.com>
2019-04-03 16:14:06 +0200

    added king movement

--- lib/king.rb
+++ lib/king.rb
@@ -13,4 +13,18 @@ class King < Piece
 			@value = "K".red
 		end
 	end
+	
+	def valid_move?(hash, next_position)
+		next_position_column = next_position[0]
+		next_position_row = next_position[1].to_i
+		current_position_column = @position[0]
+		current_position_row = @position[1].to_i
+		
+		if ((current_position_column.ord - next_position_column.ord).abs <= 1 && (current_position_row - next_position_row).abs <= 1 && (hash["#{next_position}"] == nil || hash["#{next_position}"].color != self.color))
+			@position = next_position
+			return true
+		end
+		
+		return false
+	end
 end
